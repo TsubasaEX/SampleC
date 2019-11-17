d0efd5b039ef04767cf14b07cb8f3895c51b717a
TsubasaEX <ss3808924@gmail.com>
2019-11-17 17:23:49 +0800

    up

---
+++ 8-class/Untitled1.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+	public:
+		Complex();
+		Complex(int r,int i);
+		Complex operator+(const Complex&);
+		Complex operator-(const Complex&);
+	 	void operator+=(const Complex&);
+	 	void operator-=(const Complex&);
+		void print();
+		void set(int r,int i);
+		bool operator==(const Complex&);
+	private:
+		int real;
+		int image;
+};
+
+Complex::Complex(){
+	real=0;
+	image=0;
+}
+
+Complex::Complex(int r,int i){
+	real=r;
+	image=i;
+}
+
+Complex Complex::operator+(const Complex&c){
+	Complex x;
+	x.real=real+c.real;
+	x.image=image+c.image;
+	return x;
+}
+
+Complex Complex::operator-(const Complex&c){
+	Complex x;
+	x.real=real-c.real;
+	x.image=image-c.image;
+	return x;
+}
+
+void Complex::operator+=(const Complex&c){
+	real+=c.real;
+	image+=c.image;
+}
+
+void Complex::operator-=(const Complex&c){
+	real-=c.real;
+	image-=c.image;
+}
+
+bool Complex::operator==(const Complex&c){
+	if(real==c.real && image==c.image){
+		return true;
+	}
+	return false;
+}
+
+void Complex::print(){
+	cout<<real<<" "<<image<<endl;
+}
+
+void Complex::set(int r,int i){
+	real=r;
+	image=i;
+}
+
+
+int main(){
+	
+	Complex c1;
+	Complex c2(5,10);
+	Complex c3;
+	c1.print();
+	c2.print();
+	
+	c3=c1+c2;
+	c3.print();
+	
+	c2+=c3;
+	c2.print();
+	
+	c3-=c2;
+	c3.print();
+	
+	c3.set(11,22);
+	c3.print();
+	if(c3==c1){
+		cout<<"equal"<<endl;
+	}else{
+		cout<<"unequal"<<endl;
+	}
+	
+	c1=c3;
+	c1.print();
+	if(c3==c1){
+		cout<<"equal"<<endl;
+	}else{
+		cout<<"unequal"<<endl;
+	}
+	return 0;
+}
